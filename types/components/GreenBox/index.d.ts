150760118d9d0f4bb18ec8d4702e684794fccb02
SHEN <>
2020-10-28 11:18:34 +0800

    types problem

---
+++ types/components/GreenBox/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import './style.less';
+declare function BlueBox(): JSX.Element;
+export default BlueBox;
---
+++ types/utils/index.d.ts
@@ -0,0 +1,3 @@
+export declare function sayHello(text: string): string;
+export declare function sayWorld(text: string): string;
+export declare function clone(o: any): any;
